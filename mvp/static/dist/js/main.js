bcd90ffacbae0d1aca37020b6d99aa1f4ed75cac
ali-rahat <rahat.amiable@gmail.com>
2020-04-01 00:15:29 +0500

    All Json added to index page

--- mvp/static/dist/js/main.js
+++ mvp/static/dist/js/main.js
@@ -1,20 +1,4 @@
 $(function (){
-  var barChartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'Dataset 1',
-      backgroundColor: 'red',
-      data: [238, 252, 276, 513, 637, 745, 769]
-    }, {
-      label: 'Dataset 2',
-      backgroundColor: 'blue',
-      data: [214, 217, 221, 425, 427, 456, 765]
-    }, {
-      label: 'Dataset 3',
-      backgroundColor: 'green',
-      data: [52, 79, 674, 678, 784, 899, 913]
-    }]
-  };
 
   var ctx = document.getElementById('barChart').getContext('2d');
   window.myBar = new Chart(ctx, {
